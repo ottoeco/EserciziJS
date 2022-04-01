6d8610eaad0b5d1d9fa77dfb6e40eab3360aa303
Joe joe <jonby70@gmail.com>
2022-04-01 15:40:56 +0200

    add pari-dispari jquery

--- 2022_04_01/sei.js
+++ 2022_04_01/sei.js
@@ -1,1 +1,7 @@
 console.log('lezione 6 - jquery');
+$('#mio_bottone').text('Viva Jquery').css('color','blue')
+
+//seleziono le righe pari e dispari della tabella 
+//e aggiungo la classe
+$("tr:even").addClass("table-primary")
+$("tr:odd").addClass("table-secondary")
