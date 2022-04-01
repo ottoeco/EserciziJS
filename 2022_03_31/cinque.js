d3815f97a1553ffc39a884a76ae12b7baacb78ff
Joe joe <jonby70@gmail.com>
2022-04-01 15:08:39 +0200

    fine lez5

--- 2022_03_31/cinque.js
+++ 2022_03_31/cinque.js
@@ -2,12 +2,13 @@ console.log('lezione 5 - OOP -TIPI- PROTOTIPI');
 //prima versione (old) è
 function utente(nome,cognome)
 {
-  let u = Object.create(utente.methods)
+  let u = Object.create(utente.metodi)
   u.nome = nome
   u.cognome = cognome
   return u 
 }
-utente.methods = { 
+utente.metodi =
+{ 
     //toString() { return "Utente: " + this.nome +" "+ this.cognome }
     toString1: function(){},
     toString2: ()=>{
@@ -29,8 +30,48 @@ function Utente2(nome,cognome)
   this.cognome = cognome
 }
 Utente2.prototype={
-    toString() { return "Utente: " + this.nome +" "+ this.cognome }
+    toString() { return "Utente2: " + this.nome +" "+ this.cognome }
 }
 
 let u2= new Utente2("Joseph",'Renzi')
 console.log(u2.toString());
+
+/*tre 
+metodi dentro, NO virgole*/
+
+class Utente3{
+    constructor(nome,cognome){
+        this.nome = nome
+        this.cognome = cognome
+    }
+    toString() { return "Utente3: " + this.nome +" "+ this.cognome }
+}
+
+let u3= new Utente3("Joseph",'Renzi')
+console.log(u3.toString());
+
+/* 9.4 */
+
+/* ABBIAMO MODIFICATO AL VOLO LA CLASSE aggiungendo un metodo 
+e lui le aggiorna su tutto le istanze attuali, su tutte le classi,
+anche quelle dichiarate in precedenza*/
+
+Utente2.prototype.spiegami=()=>{ return "Utente2: messaggio per tutti" }
+
+//ES6
+class EZArray extends Array {
+    get first() { return this[0]; }
+    get last() { return this[this.length-1]; }
+}
+let v=new EZArray();
+v.push(1)
+
+/*
+https://www.tiobe.com/tiobe-index/
+https://pypl.github.io/PYPL.html
+
+ES 2017- modifica delle Promise
+async ->davanti alla funzione da richiamare
+await ->aspetta la chiamata delle async
+    punti di attesa, per la fetch
+*/
